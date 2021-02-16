c37c3399ece21a03f3d7f88b0a67c052aeac34e0
rodnxt <59828243+rodnxt@users.noreply.github.com>
2021-02-16 18:52:32 +0100

    update
    ok

---
+++ Base.py
@@ -0,0 +1,77 @@
+import pysfmov
+import matplotlib.pyplot as plt
+import numpy as np
+import analisi_tsa # modulo
+
+import RoiSelect
+
+
+fa = 100 # freq ac1uisiszione
+fr = 35
+path_base = r'C:/Users/Rodo/Dropbox/Il mio PC (LAPTOP-SA2HR7TC)/Desktop/Tesi/Dati/'
+path = path_base + f'cfrp{fr}hz.sfmov'
+path_npy = path_base + f'cpfr{fr}.npy'
+
+flag = False # da ritagliare .npy? o video interno
+if flag:
+    Analisi = analisi_tsa.TSA(fa,path)
+    cordinate = RoiSelect.selectROI(Analisi.finestra_video[:,:,0],titolo='Seleziona Area Analisi')
+    Analisi.taglio_video(cordinate[1],cordinate[0],cordinate[3],cordinate[2],save=True,view = True,save_path=path_npy)
+else:
+    Analisi = analisi_tsa.TSA(fa,path_npy)
+
+
+cordinate = RoiSelect.selectROI(Analisi.finestra_video[:,:,0],titolo='Seleziona ROI')
+Analisi.selezione_ROI(cordinate[1],cordinate[0],cordinate[3],cordinate[2],view = True)
+
+
+cordinate = RoiSelect.selectROI_ellipse(Analisi.finestra_video_roi_media,titolo='Seleziona ROI foro')
+Analisi.selezione_ROI_foro(cordinate[1],cordinate[0],cordinate[3],cordinate[2])
+
+cordinate = RoiSelect.selectROI(Analisi.finestra_video_roi_media,titolo='Seleziona finestra di compensazione')
+print(cordinate)
+Analisi.selezione_ROI_compensazione(cordinate[1],cordinate[0],cordinate[3],cordinate[2],fr,df = 5,view=True)
+
+
+
+
+#Analisi.view()
+#Analisi.view_animate()
+
+(mappa_modulo,mappa_fase)=Analisi.ottieni_mappe_lockin(view = True,t_lim_inf = None, t_lim_sup = None)
+#Analisi.ottieni_mappe_lockin(fr=fr,view = True,t_lim_inf = None, t_lim_sup = None)
+cordinate = RoiSelect.select_line(mappa_modulo,titolo='Seleziona res')
+Analisi.view_result_line(cordinate[1],cordinate[0],cordinate[3],cordinate[2])
+
+from matplotlib.widgets import Slider
+
+def update(val):
+    lim_inf = cmap_lim_inf.val
+    lim_sup = cmap_lim_sup.val
+    if lim_inf<lim_sup:
+        l.set_clim([lim_inf,lim_sup])
+    fig.canvas.draw_idle()
+
+
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(left=0.25, bottom=0.25)
+lim_inf = 1.1*np.min(mappa_fase)
+lim_sup = 0.9*np.max(mappa_fase)
+l = plt.imshow(np.abs(np.abs(mappa_fase)-np.mean(mappa_fase)*np.ones(mappa_fase.shape)),cmap='magma')
+fig.colorbar(l,orientation = 'vertical',fraction = 0.5)
+ax.margins(x=0)
+
+axcolor = 'lightgoldenrodyellow'
+ax_lim_inf = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+ax_lim_sup = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+
+cmap_lim_inf = Slider(ax_lim_inf, 'sunf', np.min(mappa_fase), np.max(mappa_fase), valinit=lim_inf)
+cmap_lim_sup = Slider(ax_lim_sup, 'sup', np.min(mappa_fase), np.max(mappa_fase), valinit=lim_sup)
+cmap_lim_inf.on_changed(update)
+cmap_lim_sup.on_changed(update)
+
+plt.show()
+
+
+
---
+++ RoiSelect.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.widgets import RectangleSelector,EllipseSelector,Slider
+
+cordinate = (0,0,0,0)
+
+
+def __onselect(eclick, erelease):
+    "eclick and erelease are matplotlib events at press and release."
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+    print("(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2))
+
+    global cordinate
+    cordinate = (int(x1),int(y1),int(x2),int(y2))
+
+def __toggle_selector(event):
+    print(' Key pressed. q to exit, a activeted/ w deactived select')
+    if event.key in ['A', 'a'] and __toggle_selector.RS.active:
+        print(' Selector deactivated.')
+        __toggle_selector.RS.set_active(False)
+    if event.key in ['W', 'w'] and not __toggle_selector.RS.active:
+        print(' Selector activated.')
+        __toggle_selector.RS.set_active(True)
+
+
+def selectROI(matrice_immagine,titolo='Immagine'):
+    ''' 
+    input:
+        ax --> axis su cui fare la selezione della roi
+    output:
+        (xi,yi,dx,dy)
+    '''
+    _,ax = plt.subplots()
+    ax.imshow(matrice_immagine,cmap = 'inferno')
+    ax.set(title=titolo)
+    __toggle_selector.RS = RectangleSelector(ax, __onselect,
+                                       drawtype='box', useblit=True,
+                                       button=[1, 3],  # don't use middle button
+                                       minspanx=5, minspany=5,
+                                       spancoords='pixels',
+                                       interactive=True)
+    plt.connect('key_press_event', __toggle_selector)
+    plt.show()
+    return cordinate
+
+def select_line(matrice_immagine,titolo='Immagine'):
+    ''' 
+    input:
+        ax --> axis su cui fare la selezione della roi
+    output:
+        (xi,yi,dx,dy)
+    '''
+    _,ax = plt.subplots()
+    ax.imshow(matrice_immagine)
+    ax.set(title=titolo)
+
+    __toggle_selector.RS = RectangleSelector(ax, __onselect,
+                                       drawtype='line', useblit=True,
+                                       button=[1, 3],  # don't use middle button
+                                       minspanx=5, minspany=5,
+                                       spancoords='pixels',
+                                       interactive=True)
+    plt.connect('key_press_event', __toggle_selector)
+    plt.show()
+    return cordinate
+
+def selectROI_ellipse(matrice_immagine,titolo='Immagine'):
+    ''' 
+    input:
+        ax --> axis su cui fare la selezione della roi
+    output:
+        (x1,y1,x2,y2)
+    '''
+    _,ax = plt.subplots()
+    ax.imshow(matrice_immagine)
+    ax.set(title=titolo)
+    __toggle_selector.ES = EllipseSelector(ax, __onselect,
+                                       drawtype='box', useblit=True,
+                                       button=[1, 3],  # don't use middle button
+                                       minspanx=5, minspany=5,
+                                       spancoords='pixels',
+                                       interactive=True)
+    plt.connect('key_press_event', __toggle_selector)
+    plt.show()
+    return cordinate
+
+if __name__ == '__main__':
+    print('Modulo per roi interattiva')
+
